40d4aa250dd2e46870a0e61c66a8622a9c64bd2b
cmtthomas <gnuman1983@outlook.com>
2022-10-22 17:07:35 -0700

    more updates

--- src/ProTip.js
+++ src/ProTip.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Link from '@mui/material/Link';
+//import Link from '@mui/material/Link';
 import SvgIcon from '@mui/material/SvgIcon';
 import Typography from '@mui/material/Typography';
 
@@ -14,9 +14,8 @@ function LightBulbIcon(props) {
 export default function ProTip() {
   return (
     <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
-      <LightBulbIcon sx={{ mr: 1, verticalAlign: 'middle' }} />
-      Pro tip: See more <Link href="https://mui.com/getting-started/templates/">templates</Link> on
-      the MUI documentation.
+      <LightBulbIcon sx={{ mr: 1, verticalAlign: 'left' }} />
+      Moving towards a low carbon future, one small decision at a time
     </Typography>
   );
 }

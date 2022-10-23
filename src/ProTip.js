b610cd93bcaed6128075e8e625a8bcec4fe32737
cmtthomas <gnuman1983@outlook.com>
2022-10-23 09:08:40 -0700

    Added Dialog Box

--- src/ProTip.js
+++ src/ProTip.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+/*import * as React from 'react';
 import axios from "axios";
 import TextField from '@mui/material/TextField';
 
@@ -34,7 +34,81 @@ export default function ProTip() {
         
         </div>
     );
+} */
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import PersonIcon from '@mui/icons-material/Person';
+import AddIcon from '@mui/icons-material/Add';
+import Typography from '@mui/material/Typography';
+import { blue } from '@mui/material/colors';
+import Box from '@mui/material/Box';
+import CarbonTable from './table';
+import {today} from './config'
+import { positions } from '@mui/system';
+
+const emails = ['username@gmail.com', 'user02@gmail.com'];
+
+/*SimpleDialogProps {
+  open;
+  selectedValue;
+  onClose: (value) => void;
+} */
+
+function SimpleDialog(props) {
+  const { onClose, selectedValue, open } = props;
+
+  const handleClose = () => {
+    onClose(selectedValue);
+  };
+
+  const handleListItemClick = (value) => {
+    onClose(value);
+  };
+
+  return (
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Sources of Carbon Emissions</DialogTitle>
+        <CarbonTable path="/tracker/rows/" date={today()}/>
+        
+    </Dialog>
+  );
 }
+
+export default function SimpleDialogDemo() {
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState(emails[1]);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+   // setSelectedValue(value);
+  };
+
+  return (
+    <Box alignItems="flex-end" sx={{ my: 1}}>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add
+      </Button>
+      <SimpleDialog
+        selectedValue={selectedValue}
+        open={open}
+        onClose={handleClose}
+      />
+    </Box>
+  );
+}
+
 
 
 //<TextField id="outlined-basic" label={firstName} variant="outlined" value={firstName} onChange={callbackTest} /> 

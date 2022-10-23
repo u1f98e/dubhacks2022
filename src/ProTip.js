54f6a8381919ee6bb9f2d102b9cef24fa2ebffb0
cmtthomas <gnuman1983@outlook.com>
2022-10-23 00:36:13 -0700

    variable prints to console

--- src/ProTip.js
+++ src/ProTip.js
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 
+
+
 export default function ProTip() {
-return (<TextField id="outlined-basic" label="Outlined" variant="outlined" /> );
-};
+    const [firstName, setFirstName] = React.useState('');
+    
+    return (
+        <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" alue={firstName}   name="firstName" onChange={e => setFirstName(e.target.value)} /> 
+        <div> CustomerName: {firstName} </div>
+        </div>
+    );
+}

23c54e31746f281a6a891f618227695f7cc62c59
maritza <maritzaochoa011@gmail.com>
2022-10-23 08:07:22 -0700

    added an extra tab

--- src/panels.js
+++ src/panels.js
@@ -27,19 +27,21 @@ return (
         >
         <Tab value="/1" label="Carbon Footprint Assesment"wrapped/>
 
+        
+        <Tab value="/2" label="About your scores"wrapped/>
 
-        <Tab value="/2" label="Today's Carbon Steps" />
-
-
-
-        <Tab value="/3" label="This Month's Carbon Steps" />
-
-
-        <Tab value="/4" label="Yearly Carbon Steps" />
+        <Tab value="/3" label="Today's Carbon Steps" />
 
 
 
-        <Tab value="/5" label="About Page" />
+        <Tab value="/4" label="This Month's Carbon Steps" />
+
+
+        <Tab value="/5" label="Yearly Carbon Steps" />
+
+
+
+        <Tab value="/6" label="About Page" />
       
       </Tabs>
 
@@ -52,13 +54,16 @@ return (
                 3. 
                 4. 
              </Box>} />
-            <Route path="/2" element={<Box> {date}</Box> } />
-          <Route path="/3" element={<Box> Month {current.getMonth()+1}</Box>} />
-          <Route path="/4" element={<Box> Year {year}</Box>} />
-          <Route path="/5" element={<Box sx={{ my: 1 }}> 
+             <Route path="/2" element={<Box> About your score</Box>} />
+            <Route path="/3" element={<Box> {date}</Box> } />
+          <Route path="/4" element={<Box> Month {current.getMonth()+1}</Box>} />
+          <Route path="/5" element={<Box> Year {year}</Box>} />
+          <Route path="/6" element={<Box sx={{ my: 1 }}> 
          
-
-          footprintMinder helps user see how much of an effect they have on the enviornment in day to day tasks. They are able to get their current carbon footprint score and have a reachable goal they can work towards. 
+          FootprintMinder goal is for its users to have some sense of control when it comes to the climate crisis. In FootprintMinder you are able to see how many carbon footprints you are contributing to the climate crisis to help 
+          the user get some sort of idea of how much their daily habits are affecting our planet. FootprintMinder will give you suggestions on how you 
+          could reduce you carbon footprint and how much of a difference you would actually be making. 
+          FootprintMinder helps users see how much of an effect they have on the enviornment in day to day tasks. They are able to get their current carbon footprint score and have a reachable goal they can work towards. 
           Users can work towards a reachable yearly carbon footprint goal by following recomendations suggested by the app
           if their score for an individual day or week is too high. 
 

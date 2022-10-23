c7c3092b24e9e2316c09c0adb4c743310daf9ae4
u1f98e <u1f98e@gmail.com>
2022-10-23 08:05:32 -0700

    Make CarbonTable accept properties

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const url = "http://localhost:5000"
+export const url = "http://localhost:5000"
 
-function today() { return Date().toJSON().slice(0,10) }
-function this_month_year() { return Date().toJSON().slice(0,7) }
-function this_year() { return Date().toJSON().slice(0,4) }
+export function today() { return (new Date).toISOString().slice(0,10) }
+export function this_month_year() { return (new Date).toISOString().slice(0,7) }
+export function this_year() { return (new Date).toISOString().slice(0,4) }
--- src/table.js
+++ src/table.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import axios from "axios";
 import TextField from '@mui/material/TextField';
-// import {url, today} from "./config"
+import {url, today} from "./config"
 
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -11,69 +11,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-const url = "http://localhost:5000"
-
-function today() { return (new Date).toISOString().slice(0,10) }
-function this_month_year() { return (new Date).toISOString().slice(0,7) }
-function this_year() { return (new Date).toISOString().slice(0,4) }
-
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-// export default function BasicTable() {
-//   return (
-//     <TableContainer component={Paper}>
-//       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Dessert (100g serving)</TableCell>
-//             <TableCell align="right">Calories</TableCell>
-//             <TableCell align="right">Fat&nbsp;(g)</TableCell>
-//             <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-//             <TableCell align="right">Protein&nbsp;(g)</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {rows.map((row) => (
-//             <TableRow
-//               key={row.name}
-//               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//             >
-//               <TableCell component="th" scope="row">
-//                 {row.name}
-//               </TableCell>
-//               <TableCell align="right">{row.calories}</TableCell>
-//               <TableCell align="right">{row.fat}</TableCell>
-//               <TableCell align="right">{row.carbs}</TableCell>
-//               <TableCell align="right">{row.protein}</TableCell>
-//             </TableRow>
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </TableContainer>
-//   );
-// }
-
-export default function CarbonTable() {
+export default function CarbonTable(props) {
 	const [loaded, setLoaded] = React.useState(false)
 	const [request, setRequest] = React.useState(false)
 	const [response, setResponse] = React.useState({})
-
-	const [selectedDate, setSelectedDate] = React.useState(today())
 	
 	if(!request) {
 		axios({
 		  method: "GET",
-		  url: url + "/tracker/rows/" + selectedDate
+		  url: url + props.path + props.date
 		})
 		.then((resp) => {
 			setResponse(resp.data)
